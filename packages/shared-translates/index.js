2ded4889d2a815cb40b63fcd9ca0de0f7b96f883
Waldrey <waldrey22@gmail.com>
2024-09-17 21:48:20 -0300

    feat: creating shared-translates to vue and react native [PPP-00]

---
+++ packages/shared-translates/index.js
@@ -0,0 +1,13 @@
+// packages/shared-translates/index.js
+
+import en from './en.json';
+import pt from './pt.json';
+import es from './es.json';
+
+const translations = {
+  en,
+  pt,
+  es
+};
+
+export default translations;
